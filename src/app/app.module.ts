faad6059e847be2093aa77073578043a94a3f20b
Shreeja Patel <shreeja@icanopii.com>
2024-12-15 10:55:20 -0700

    Resolve chunk load error on production

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -8,11 +8,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { GlobalErrorHandler } from './shared/services/global-error-handler.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideAnimations()],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, 
+    {provide: ErrorHandler, useClass: GlobalErrorHandler},
+    provideAnimations()
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
